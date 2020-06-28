c8acdedf9cbf636915ab340a717c4c13f730e117
sigkill <janet@sigkill.ninja>
2020-06-28 14:30:36 -0500

    modified selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -3,8 +3,12 @@ public class SelectionSort {
     /**
      * The method for sorting the numbers
      */
+    public static int totalComparisons = 0;
+    public static int totalMovements = 0;
+    
     public static void selectionSort(double[] list) {
         for (int i = 0; i < list.length - 1; i++) {
+
             // Find the minimum in the list[i..list.length-1]
             double currentMin = list[i];
             int currentMinIndex = i;
@@ -22,6 +26,42 @@ public class SelectionSort {
                 list[i] = currentMin;
             }
         }
+        totalComparisons++;
+    	totalMovements++;
+    }
+    
+    public static void selectionSortMod(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+        	totalComparisons++;
+        	totalMovements++;
+            // Find the minimum in the list[i..list.length-1]
+            double currentMin = list[i];
+            totalMovements++;
+            int currentMinIndex = i;
+            totalMovements++;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                    totalMovements += 2;
+                }
+                totalComparisons += 2;
+                totalMovements++;
+            }
+            totalMovements++;
+            totalComparisons++;
+
+            // Swap list[i] with list[currentMinIndex] if necessary;
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+                totalMovements += 2;
+            }
+            totalComparisons++;
+        }
+    	totalComparisons++;
+    	totalMovements++;
     }
 
     public static void main(String[] args) {
