e8741b9adc7b49899886c0cc13cb71c98201d120
Jacob-aa-garcia <47547652+Jacob-aa-garcia@users.noreply.github.com>
2020-07-19 10:27:19 -0500

    Update Main.java
    Added checking AVL and BST properties after inserting into randomTree

--- Projects/AVLTree/src/Main.java
+++ Projects/AVLTree/src/Main.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+
 public class Main {
+	public static int sizeOfRandomTree = 100;
 	
     public static void main(String[] args) {
         AVLTree<Long, Book> avlTree = new AVLTree<>();
@@ -24,10 +26,20 @@ public class Main {
             System.out.println("complexity: " + avlTree.complexity);
             // shuffle list for Random binary tree
             Collections.shuffle(books);		
-            for (Book b : books) 
-            	randomTree.insert(randomTree.root, b.ISBN, b);
+            for (int i = 0; i < sizeOfRandomTree; i++) 
+            	randomTree.insert(books.get(i).ISBN, books.get(i));
             books.clear();
             sc.close();
+            randomTree.printInOrder(randomTree.root);
+            System.out.println("Random Tree size: " + sizeOfRandomTree);
+            if (randomTree.isBSTOrder(randomTree.root, Long.MIN_VALUE, Long.MAX_VALUE)) {
+            		System.out.println("BST order: true");
+            	if (randomTree.isAVLStructure(randomTree.root)) 
+            		System.out.println("AVL structure: true");
+
+            }
+            else
+            	System.out.println("BST order: false\nAVL structure: false" );
         } catch (IOException e) {
             e.printStackTrace();
         }
