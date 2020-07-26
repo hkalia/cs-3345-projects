217d309cf89af545c76bb86a9d1b27a401cbd916
Jacob-aa-garcia <47547652+Jacob-aa-garcia@users.noreply.github.com>
2020-07-26 18:35:20 -0500

    Update CityData.java
    Moved cost and known here to work better with Dijkstra Algorithm

--- Projects/flightPlan/src/CityData.java
+++ Projects/flightPlan/src/CityData.java
@@ -1,10 +1,16 @@
 public class CityData {
     String cityName;
     LinkedList<ConnectionData> connections;
-
+    Boolean known;
+    Integer cost;
+    Node<CityData> path;
+    
     CityData(String cityName, ConnectionData connections) {
         this.cityName = cityName;
         this.connections = new LinkedList<>(connections);
+        this.known = false;
+        this.cost = Integer.MAX_VALUE;
+        this.path = null;
     }
 
     @Override
