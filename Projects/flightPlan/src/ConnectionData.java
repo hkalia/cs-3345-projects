0e974fd6ed63d0a61b3b08f4ec86e296fd8eb158
Jacob-aa-garcia <47547652+Jacob-aa-garcia@users.noreply.github.com>
2020-07-26 18:36:13 -0500

    Update ConnectionData.java
    Removed known and cost variables as they are not needed

--- Projects/flightPlan/src/ConnectionData.java
+++ Projects/flightPlan/src/ConnectionData.java
@@ -1,24 +1,19 @@
 public class ConnectionData {
-    String cityName;
-    double dollarCost;
+	String cityName;
+    int pennyCost;
     int minuteCost;
-    Integer cost;
-    boolean known;
 
     ConnectionData(String cityName, double dollarCost, int minuteCost) {
         this.cityName = cityName;
-        this.dollarCost = dollarCost;
+        this.pennyCost = (int) dollarCost * 100;
         this.minuteCost = minuteCost;
-        this.cost = null;
-        this.known = false;
     }
 
     @Override
     public String toString() {
         return "ConnectionData{" + this.cityName +
-                ", " + this.dollarCost +
+                ", " + (double) this.pennyCost/100 +
                 ", " + this.minuteCost +
-                ", " + this.cost +
-                ", " + this.known + "}";
+                "}";
     }
 }
