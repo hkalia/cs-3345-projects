4da4b80b8dded74a066937395f73e1b69b2ab1f3
sigkill <janet@sigkill.ninja>
2020-07-27 16:03:25 -0500

    file output

--- Projects/flightPlan/src/Main.java
+++ Projects/flightPlan/src/Main.java
@@ -1,12 +1,20 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         FlightGraph flightGraph = new FlightGraph();
-
+        PrintStream fileOut = null;
+		try {
+			fileOut = new PrintStream(args[2]);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+        System.setOut(fileOut);
         try {
             FileReader flightDataReader = new FileReader(args[0]);
             FileReader requests = new FileReader(args[1]);
@@ -22,7 +30,7 @@ public class Main {
             sc = new Scanner(requests);
 
             while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            	System.out.println(sc.nextLine());
             }
             sc.close();
         } catch (IOException e) {
