8fcf721a582bf69188e0ad5008b1b773d2c8ecff
sigkill <janet@sigkill.ninja>
2020-07-27 16:04:48 -0500

    format

--- Projects/flightPlan/src/Main.java
+++ Projects/flightPlan/src/Main.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        FlightGraph flightGraph = new FlightGraph();
-        PrintStream fileOut = null;
+	public static void main(String[] args) {
+		FlightGraph flightGraph = new FlightGraph();
+		PrintStream fileOut = null;
 		try {
 			fileOut = new PrintStream(args[2]);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-        System.setOut(fileOut);
-        try {
-            FileReader flightDataReader = new FileReader(args[0]);
-            FileReader requests = new FileReader(args[1]);
-            Scanner sc = new Scanner(flightDataReader);
+		System.setOut(fileOut);
+		try {
+			FileReader flightDataReader = new FileReader(args[0]);
+			FileReader requests = new FileReader(args[1]);
+			Scanner sc = new Scanner(flightDataReader);
 
-            sc.nextLine();
-            while (sc.hasNextLine()) {
-                flightGraph.addEdge(sc.nextLine().split("\\|", 4));
-            }
-            sc.close();
-            flightGraph.print();
+			sc.nextLine();
+			while (sc.hasNextLine()) {
+				flightGraph.addEdge(sc.nextLine().split("\\|", 4));
+			}
+			sc.close();
+			flightGraph.print();
 
-            sc = new Scanner(requests);
+			sc = new Scanner(requests);
 
-            while (sc.hasNextLine()) {
-            	System.out.println(sc.nextLine());
-            }
-            sc.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+			sc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
